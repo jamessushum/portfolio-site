fe63983bfb19a92a84cce7a93c6110b8ccbd2c5a
James Su-Shum <sushumjames@gmail.com>
2020-09-06 14:49:22 -0400

    Modified structure in Hero and social icons in SocialLinks

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,47 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h4>Hi, my name is</h4>
+            <h1>James Su-Shum.</h1>
+            <h4>
+              I'm a software developer specializing in building meaningful and
+              functional applications.
+            </h4>
+            <Link to="/contact" className="btn">
+              See Projects
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        {/* <Image fluid={fluid} className="hero-img" /> */}
+      </div>
+    </header>
+  )
 }
 
 export default Hero
