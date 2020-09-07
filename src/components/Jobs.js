bcb16e5878bc5e554695e14947fe25a74e73a817
James Su-Shum <sushumjames@gmail.com>
2020-09-06 21:55:19 -0400

    Modified Experience section using Strapi as API

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,71 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = useState(0)
+  const { company, date, position, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="Experience" />
+      <div className="jobs-center">
+        {/* Button Container */}
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {job.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* Job Info */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        Download Resume
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
