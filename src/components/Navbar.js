7b40d1fe6e2696d8aed5ace83977f2d2ebb56857
James Su-Shum <sushumjames@gmail.com>
2020-09-12 16:47:18 -0400

    Added footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="logo" />
+          {/* <img src={logo} alt="logo" /> */}
           <button type="button" className="toggle-btn">
             <FaAlignRight></FaAlignRight>
           </button>
