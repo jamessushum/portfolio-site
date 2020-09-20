c3b84d78bb332b90fa441f187d6e6267f995f770
James Su-Shum <sushumjames@gmail.com>
2020-09-20 16:52:39 -0400

    Populated contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,16 @@ import Title from "./Title"
 
 const Contact = ({ title }) => {
   return (
-    <section className="section projects" id="projects">
+    <section className="section projects" id="contact">
       <Title title={title} />
+      <div className="contact-body">
+        <article className="contact">
+          <h4>Have any questions or just want to say hi, send me a message!</h4>
+          <a href="mailto:sushumjames@gmail.com" className="btn center-btn">
+            Say Hello
+          </a>
+        </article>
+      </div>
     </section>
   )
 }
